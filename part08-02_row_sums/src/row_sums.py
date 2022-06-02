87f2cd8f922ed0702cd7c31c2fa85c8944664077
Genaro Di Stilio <genarodistilio@gmail.com>
2022-06-02 15:26:30 -0300

    Teste exercise2

--- part08-02_row_sums/src/row_sums.py
+++ part08-02_row_sums/src/row_sums.py
@@ -1,1 +1,9 @@
 # Write your solution here
+def row_sums(my_matrix: list):
+    for lista in my_matrix:
+        lista.append(sum(lista))
+
+if __name__ == "__main__":
+    my_matrix = [[1, 2], [3, 4]]
+    row_sums(my_matrix)
+    print(my_matrix)
