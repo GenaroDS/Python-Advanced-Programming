ac0859719c4a972df7280a86eed0c219ea759f8e
Genaro Di Stilio <genarodistilio@gmail.com>
2022-06-02 13:29:05 -0300

    initializing repository

---
+++ part08-09_books_of_genre/src/books_of_genre.py
@@ -0,0 +1,17 @@
+# ÄLÄ MUUTA ALLA OLEVAA LUOKKAA Kirja
+# Kirjoita ratkaisui Kirja-luokan jälkeen
+
+class Book:
+    def __init__(self, name: str, author: str, genre: str, year: int):
+        self.name = name
+        self.author = author
+        self.genre = genre 
+        self.year = year
+
+    ##STUB:# This enables easy printing of a Book object
+    def __repr__(self):
+        return f"{self.name} ({self.author}), {self.year} - genre: {self.genre}"
+
+
+# -----------------------------
+# tee ratkaisu tänne
