544929037455bfb044dec647bf90fe663c8a55f6
Genaro Di Stilio <genarodistilio@gmail.com>
2022-06-05 15:49:48 -0300

    Part 8-4

--- part08-11_first_and_last_name/src/first_and_last_name.py
+++ part08-11_first_and_last_name/src/first_and_last_name.py
@@ -1,6 +1,14 @@
 # Write your solution here:
 
-
+class Person:
+    def __init__(self, name :str):
+        self.name = name
+    def return_first_name(self):
+        splitted = self.name.split(" ")
+        return splitted[0]
+    def return_last_name(self):
+        splitted = self.name.split(" ")
+        return splitted[1]
 
 
 
--- part08-12_number_stats/src/number_stats.py
+++ part08-12_number_stats/src/number_stats.py
@@ -1,10 +1,39 @@
 # Write your solution here!
-class  NumberStats:
+class NumberStats:
     def __init__(self):
         self.numbers = 0
-
+        self.counter = 0
     def add_number(self, number:int):
-        pass
-
+        self.numbers += number
+        self.counter += 1
+        
     def count_numbers(self):
-        pass
+        return self.counter
+        
+    def get_sum(self):
+        if self.counter>0:
+            return self.numbers
+        return 0
+    def average(self):
+        if self.counter>0:
+            return self.numbers/self.counter
+        return 0
+
+
+numbers = NumberStats()
+odd_numbers = NumberStats()
+even_numbers = NumberStats()
+print("please type in integer numbers")
+while True:
+    number = int(input())
+    if number == -1:
+        break
+    numbers.add_number(number)
+    if number % 2 == 0:
+        even_numbers.add_number(number)
+    else:
+        odd_numbers.add_number(number)
+print(f"Sum of numbers: {numbers.get_sum()}")
+print(f"Mean of numbers: {numbers.average()}")
+print(f"Sum of even numbers: {even_numbers.get_sum()}")
+print(f"Sum of odd numbers: {odd_numbers.get_sum()}")
