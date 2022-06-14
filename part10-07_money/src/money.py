3b03a275b89f15f3bfd8ee5f0833918cf90fb1f5
Genaro Di Stilio <genarodistilio@gmail.com>
2022-06-14 16:58:34 -0300

    Finished 7,8,9 of part 10

--- part10-07_money/src/money.py
+++ part10-07_money/src/money.py
@@ -1,8 +1,44 @@
 # TEE RATKAISUSI TÄHÄN:
 class Money:
     def __init__(self, euros: int, cents: int):
-        self.euros = euros
-        self.cents = cents
+        self._euros = euros
+        self._cents = cents
 
     def __str__(self):
-        return f"{self.euros}.{self.cents}"
+        money = (self._euros)*100+(self._cents)     
+        money =  money/100
+        return f"{money:.2f} eur"
+         #Arreglar
+    @property
+    def _float(self):
+        money = (self._euros)*100+(self._cents)      
+        money =  money/100
+        return money
+
+    def __eq__(self, another) -> bool:
+        return self._float == another._float
+
+    def __lt__(self, another):
+        return self._float < another._float
+
+    def __gt__(self, another):
+        return self._float > another._float
+
+    def __ne__(self,another):
+        return self._float != another._float
+
+    def __add__(self,another):
+
+        #Arreglar
+        pesos = self._euros + another._euros
+        centavos = self._cents + another._cents        
+        return Money(pesos,centavos)
+
+    def __sub__(self,another):
+        if self._float - another._float < 0:
+            raise ValueError
+        else:
+            newMoney = round(self._float-another._float,2)
+            newMoney = f"{newMoney:.2f}"
+            newMoney = newMoney.split(".")
+            return Money(int(newMoney[0]),int(newMoney[1]))
--- part10-08_simple_date/src/simple_date.py
+++ part10-08_simple_date/src/simple_date.py
@@ -1,2 +1,62 @@
 # TEE RATKAISUSI TÄHÄN:
+import datetime
+from operator import truediv
+class SimpleDate:
+    def __init__(self, day:int, month:int , year:int):
+        self._day = day
+        self._month = month
+        self._year = year
+    
+    def __str__(self):
+        return f"{self._day}.{self._month}.{self._year}"
 
+    def __lt__(self, another):
+        if self._year < another._year:
+            return True
+        elif self._year > another._year:
+            return False
+        if self._month < another._month:
+            return True
+        elif self._month > another._month:
+            return False
+        if self._day < another._day:
+            return True
+        elif self._day > another._day:
+            return False
+    def __gt__(self, another):
+        if self._year < another._year:
+            return False
+        elif self._year > another._year:
+            return True
+        if self._month < another._month:
+            return False
+        elif self._month > another._month:
+            return True
+        if self._day < another._day:
+            return False
+        elif self._day > another._day:
+            return True
+    def __eq__(self,another):
+        if (self._year == another._year) and (self._month == another._month) and (self._day == another._day):
+            return True
+        else:
+            return False
+
+    def __ne__(self,another):
+        return not (self == another)
+
+    def __add__(self, num : int):
+        newReturn = SimpleDate(self._day, self._month, self._year)
+        newReturn._day += num
+        while newReturn._day > 30:
+            newReturn._month += 1
+            newReturn._day -= 30
+        while newReturn._month > 12:
+            newReturn._year += 1
+            newReturn._month -= 12
+        return newReturn
+
+    def __sub__(self, another):
+        totalSelf = self._day + self._month * 30 + self._year * 360
+        anotherSelf = another._day + another._month * 30 + another._year * 360
+        return abs(totalSelf-anotherSelf)
