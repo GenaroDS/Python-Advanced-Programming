c30ff8be8024dee2fac3bb75a1cd0df707bd8272
Genaro Di Stilio <genarodistilio@gmail.com>
2022-06-21 23:27:04 -0300

    Finished part 10

--- part10-11_phone_book_v2/src/phone_book_v2.py
+++ part10-11_phone_book_v2/src/phone_book_v2.py
@@ -1,21 +1,37 @@
 
 # Write your solution here:
+
 class PhoneBook:
     def __init__(self):
         self.__persons = {}
 
-    def add_number(self, name: str, number: str):
+    def add_number(self, name :str, number : str):
         if not name in self.__persons:
-            self.__persons[name] = []
-        self.__persons[name].append(number)
+            self.__persons[name] = Person(name)
+            
+        self.__persons[name].add_number(number)
+    
+    def add_address(self, name :str, address : str):
+        if not name in self.__persons:
+            self.__persons[name] = Person(name)
+        
+        self.__persons[name].add_address(address)
 
     def get_entry(self, name: str):
         if not name in self.__persons:
             return None
-        return self.__persons[name]
+        return self.__persons[name].numbers()
+
+    def address(self, name: str):
+        if not name in self.__persons:
+            return None
+        return self.__persons[name].address()
 
     def all_entries(self):
-        return self.__persons
+        toPrint = ""
+        for key, values in self.__persons.items():    
+            toPrint += f"{key}, {values.numbers()} \n"         
+        return toPrint.strip()
 
 class PhoneBookApplication:
     def __init__(self):
@@ -26,7 +42,7 @@ class PhoneBookApplication:
         print("0 exit")
         print("1 add number")
         print("2 search")
-
+        print("3 add address")
     def add_number(self):
         name = input("name: ")
         number = input("number: ")
@@ -35,11 +51,22 @@ class PhoneBookApplication:
     def search(self):
         name = input("name: ")
         numbers = self.__phonebook.get_entry(name)
-        if numbers == None:
-            print("number unknown") 
-            return 
-        for number in numbers:
-            print(number)       
+        address = self.__phonebook.address(name)        
+        if numbers:
+            for number in numbers:
+                print(number)       
+        else:
+            print("number unknown")
+        if address == None:
+            print("address unknown")
+        else:
+            print(address)
+       
+
+    def add_address(self):
+        name = input("name: ")
+        address = input("address: ")
+        self.__phonebook.add_address(name, address)
 
     def execute(self):
         self.help()
@@ -52,10 +79,36 @@ class PhoneBookApplication:
                 self.add_number()
             elif command == "2":
                 self.search()
+            elif command == "3":
+                self.add_address()
             else:
                 self.help()
+class Person:
+    def __init__(self, name: str):
+        self._name = name
+        self._address = ""
+        self._numbers = []
+
+    def add_address(self, address : str):
+        self._address = address
+
+    def add_number(self, number: str):
+        self._numbers.append(number)
+    
+    def name(self):
+        return self._name
+
+    def address(self):
+        if self._address == "":
+            return None
+        else: 
+            return self._address
+
+    def numbers(self):
+        return self._numbers
 
 
 # when testing, no code should be outside application except the following:
 application = PhoneBookApplication()
 application.execute()
+
--- part10-12_course_records/src/course_records.py
+++ part10-12_course_records/src/course_records.py
@@ -1,1 +1,110 @@
-# tee ratkaisusi tänne
+
+class Course:
+    def __init__(self, name : str, grade : str, credits : str):
+        self.__name = name
+        self.__grade = grade
+        self.__credits = credits
+
+    def name(self):
+        return self.__name
+
+    def grade(self):
+        return self.__grade
+    
+    def credits(self):
+        return self.__credits
+
+class Courses:
+    def __init__(self):
+        self.__courses = {}
+
+    def add_course(self, name : str, grade : str, credits : str):
+        if not name in self.__courses:
+            self.__courses[name] = Course(name,grade,credits)
+        elif name in self.__courses:
+            if grade > self.__courses[name].grade():
+                self.__courses[name] = Course(name,grade,credits)
+
+    def search_course(self, name: str):
+        if name in self.__courses:
+            return self.__courses[name]
+        else:
+            return None
+    
+    def dictionary_length(self):
+        return len(self.__courses)
+
+    def dictionary_items(self):
+        return self.__courses
+
+class CoursesApplication:
+    def __init__(self):
+        self.__application = Courses()
+
+    def add_course(self):
+        course = input("course: ")    
+        grade = input("grade: ")
+        credits = input("credits: ")    
+        self.__application.add_course(course, grade, credits)
+
+    def search_course(self):
+        course = input("course: ") 
+        toPrint = self.__application.search_course(course)
+        if toPrint == None:
+            print("no entry for this course")
+        else:
+            print(f"{toPrint.name()} ({toPrint.credits()} cr) grade {toPrint.grade()}")
+
+    def statistics(self):
+        completedCourses = self.__application.dictionary_length()
+        totalCredits = 0
+        totalGrades = 0
+        fives = ""
+        fours = ""
+        threes = ""
+        twos = ""
+        ones = ""
+        for key, values in self.__application.dictionary_items().items():
+            totalCredits += int(values.credits())
+            totalGrades += int(values.grade())
+            if int(values.grade()) == 5:
+                fives += "x"
+            elif int(values.grade()) == 4:
+                fours += "x"
+            elif int(values.grade()) == 3:
+                threes += "x"
+            elif int(values.grade()) == 2:
+                twos += "x"
+            elif int(values.grade()) == 1:
+                ones += "x"
+        print(f"{completedCourses} completed courses, a total of {totalCredits} credits")
+        toPrint = totalGrades/completedCourses
+        print(f"mean {toPrint:.1f}")
+        print("grade distribution")
+        print(f"5: {fives}")
+        print(f"4: {fours}")
+        print(f"3: {threes}")
+        print(f"2: {twos}")
+        print(f"1: {ones}")
+
+
+    def help(self):
+        print("1 add course")
+        print("2 search course")
+        print("3 statistics")
+        print("0 exit")
+    
+    def execute(self):
+        self.help()
+        while (True):
+            command = input("command: ")
+            if command == "0":
+                break
+            if command == "1":
+                self.add_course()
+            if command == "2":
+                self.search_course()
+            if command == "3":
+                self.statistics()
+aplication = CoursesApplication()
+aplication.execute()
